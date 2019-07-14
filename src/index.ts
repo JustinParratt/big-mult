ad99dac032772dbbd91ab816e5b2c2cb9e4bcc37
Justin Parratt <justinparratt@gmail.com>
2019-07-14 16:50:55 -0400

    fixed importability, arrays broken

--- src/index.ts
+++ src/index.ts
@@ -14,26 +14,28 @@ const { instantiateBuffer } = loader;
 // From a response object, i.e. as returned by window.fetch
 
 export async function giantMult(a, b, s) {
-  //const myModule = await loader.instantiateStreaming(fetch("../node_modules/wasm-big-mult/build/optimized.wasm"), {});
-
-  const wasmModule = await instantiateBuffer(
-    readFileSync("../node_modules/wasm-big-mult/build/optimized.wasm")
+  const wasmModule = await loader.instantiateStreaming(
+    fetch("../node_modules/wasm-big-mult/build/optimized.wasm"),
+    {}
   );
+  console.log(wasmModule);
   // ok so what we are gonna do is flatten the array and pass the dimensions then use them to build the result, yeet
   // the bad thing about this is that now we are boundly by roughly million by million arrays since otherwise the flattened verions will exceed
   // max array size.
   const lenA = a.length;
   const lenB = b.length;
-  const memA = wasmModule.__retain(
-    wasmModule.__allocArray(wasmModule.INT32ARRAY, a.flat())
+  const flatA = a.flat();
+  const flatB = b.flat();
+  const refA = wasmModule.__retain(
+    wasmModule.__allocArray(wasmModule.I32ARRAY, [1,2,3,4])
   );
-  const memB = wasmModule.__retain(
-    wasmModule.__allocArray(wasmModule.INT32ARRAY, b.flat())
+  const refB = wasmModule.__retain(
+    wasmModule.__allocArray([5,6,7,8])
   );
   try {
-    return wasmModule.giantMult(memA, memB, lenA, lenB, s);
+    return wasmModule.giantMult(refA, refB, lenA, lenB, s);
   } finally {
-    wasmModule.__release(memA);
-    wasmModule.__release(memB);
+    wasmModule.__release(refA);
+    wasmModule.__release(refB);
   }
 }
