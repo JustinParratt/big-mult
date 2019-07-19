64aa7583be80f28a8932640f1da65dd5a46aa97c
Justin Parratt <justinparratt@gmail.com>
2019-07-19 12:58:07 -0400

    cleaned up index a bit

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,6 @@ import { readFileSync } from "fs";
 const loader = require("assemblyscript/lib/loader");
 const { instantiateBuffer } = loader;
 
-// Fun fact, this code won't run unless you have a comment containing the word "Buffer".
-
-/*const wasmModule = instantiateBuffer(
-  readFileSync("../node_modules/wasm-big-mult/build/optimized.wasm")
-);*/
-
-// From a response object, i.e. as returned by window.fetch
-
 export async function buildMultiplier() {
   var memory = new WebAssembly.Memory({ initial: 10, maximum: 1000 });
   const wasmModule = await loader.instantiateStreaming(
@@ -24,9 +16,8 @@ export async function buildMultiplier() {
 
 export function giantMult(wasmModule, a, b, s) {
 
-  // ok so what we are gonna do is flatten the array and pass the dimensions then use them to build the result, yeet
-  // the bad thing about this is that now we are boundly by roughly million by million arrays since otherwise the flattened verions will exceed
-  // max array size.
+  // Currently flattening our array to pass it in easily to the wasm module
+  // This code will change when i find a better way to do it
   const heightA = a[0].length;
   const heightB = b[0].length;
   const flatA = a.flat();
@@ -40,7 +31,6 @@ export function giantMult(wasmModule, a, b, s) {
     wasmModule.__allocArray(wasmModule.INT32ARRAY_ID, flatB)
   );
   try {
-    const result = [];
     const arrayRefs = wasmModule.__getArray(
       wasmModule.giantMult(refA, refB, lenA, lenB, heightA, heightB, s)
     );
